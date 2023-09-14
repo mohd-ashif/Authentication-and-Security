a6c6b2440fa55b76fe414746bbffbf1ae41fc1cf
mohd-ashif <mohdaszif077@gmail.com>
2023-09-14 15:26:17 +0530

    incomplete

---
+++ app.js
@@ -0,0 +1,59 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require ("mongoose")
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+
+const userSchema ={
+    email: String,
+    password:String
+}
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", async function(req, res) {
+    const newUser = new User({
+       email: req.body.username,
+       password: req.body.password
+    });
+   
+    try {
+       await newUser.save();
+       res.render("secrets");
+    } catch (err) {
+       console.log(err);
+    }
+   });
+   
+
+
+
+
+app.listen(3000, function(){
+    console.log('server started on port 3000');
+
+})
+
+
