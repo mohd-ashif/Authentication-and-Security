557796adde89afe17f11be9d620246aa1061d74b
mohd-ashif <mohdaszif077@gmail.com>
2023-09-14 17:50:33 +0530

    add Encrypt

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require ("mongoose-encryption"); 
 
 const app = express();
 
@@ -14,10 +15,13 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect('mongodb://127.0.0.1:27017/myapp');
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+})
+
+const secret = "thisisourlittilesecret.";
+userSchema.plugin(encrypt, { secret: secret , encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
